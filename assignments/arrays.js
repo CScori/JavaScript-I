3d5d2be56f69f5714f1632ea1ac6fdc4fd8bbedf
CScori <49799016+CScori@users.noreply.github.com>
2020-01-27 14:08:10 -0800

    adds clean slate

--- assignments/arrays.js
+++ assignments/arrays.js
@@ -64,22 +64,7 @@ let inventory = [{ "id": 1, "car_make": "Lincoln", "car_model": "Navigator", "ca
 // ==== Challenge 1 ====
 // The dealer can't recall the information for a car with an id of 33 on his lot. Help the dealer find out which car has an id of 33 by logging the car's year, make, and model in the console log provided to you below:
 // console.log(`Car 33 is a *car year goes here* *car make goes here* *car model goes here*` );
-let carModels = [];
-for (let i = 0; i < inventory.length; i++)
-    carModels.push(inventory[i].car_model);
-console.log(carModels.sort());
 
-let carMakes = [];
-for (let i = 0; i < inventory.length; i++)
-    carMakes.push(inventory[i].car_make);
-console.log(carMakes.sort());
-
-let carYears = [];
-for (let i = 0; i < inventory.length; i++)
-    carYears.push(inventory[i].car_year);
-console.log(carYears.sort());
-
-console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${inventory[32].car_model}`);
 
 
 
@@ -88,21 +73,15 @@ console.log(`Car 33 is a ${inventory[32].car_year} ${inventory[32].car_make} ${i
 
 
 // console.log();
-const lastCar = 0;
+
 
 // ==== Challenge 3 ====
 // The marketing team wants the car models listed alphabetically on the website. Sort all the car model names into alphabetical order and log the results in the console
-let carModel = [];
-for (let i = 0; i < inventory.length; i++)
-    carModel.push(inventory[i].car_model);
-console.log(carModel.sort());
+
 
 // ==== Challenge 4 ====
 // The accounting team needs all the years from every car on the lot. Create a new array from the dealer data containing only the car years and log the result in the console.
-let carYear = [];
-for (let i = 0; i < inventory.length; i++)
-    carYear.push(inventory[i].car_year);
-console.log(carYears.sort());
+
 
 // ==== Challenge 5 ====
 // The car lot manager needs to find out how many cars are older than the year 2000. Using the carYears array you just created, find out how many cars were made before the year 2000 by populating the array oldCars and logging it's length.
--- assignments/function-conversion.js
+++ assignments/function-conversion.js
@@ -12,30 +12,17 @@ console.log("Function was invoked!")
 //   return param;
 // };
 // anotherFunction("Example");
-let anotherFunction = param1 => {
-    return param1;
-}
-console.log(anotherFunction("Example"))
+
 // let add = function (param1, param2) {
 //   return param1 + param2;
 // };
 // add(1,2);
-let add = (prm1,prm2) => {
-return prm1 + prm2;
-}
-console.log(add(1,2))
+
+
 // let subtract = function (param1, param2) {
 //   return param1 - param2;
 // };
 // subtract(1,2);
-let subtract = (rem1,rem2) => {
-    return rem1 - rem2;
-}
-console.log(subtract(89,22))
+
 // Stretch
 
-exampleArray = [1,2,3,4];
-const triple = exampleArray.map(function (num) {
-  return num * 3;
-});
-console.log(triple);
